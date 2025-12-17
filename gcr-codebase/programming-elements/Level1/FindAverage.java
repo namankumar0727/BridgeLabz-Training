3d3335d864e13376bb4823852a51c0e4b1c3e37b
Naman Kumar <namankumar0727@gmail.com>
2025-12-17 15:18:38 +0530

    [Naman] : added Find Average percent marks

---
+++ gcr-codebase/programming-elements/Level1/FindAverage.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class FindAverage{
+	
+	public static void main(String args[]){
+		
+		int maths=94;
+		int physics=95;
+		int chemistry=96;
+		
+		int average=(maths+physics+chemistry)/3;
+		
+		System.out.println("Average percent marks in PCM is: "+average);
+			
+	}
+
+}
