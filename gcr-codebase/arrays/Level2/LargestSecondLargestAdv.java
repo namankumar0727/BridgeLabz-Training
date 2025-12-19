59b6d98dd98c2e684e0a6f3b703a6da810bb6677
Naman Kumar <namankumar0727@gmail.com>
2025-12-20 01:29:00 +0530

    [Naman] : added largest and second largest adv

---
+++ gcr-codebase/arrays/Level2/LargestSecondLargestAdv.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class LargestSecondLargestAdv{
+public static void main(String args[]){
+
+	//Taking input of number from user
+	Scanner sc=new Scanner(System.in);
+	int number=sc.nextInt();
+	int maxDigit=10;
+	int digits[]=new int[maxDigit];
+	int index=0;
+	while(number!=0){
+	if(index==maxDigit){
+		maxDigit=maxDigit+10;
+		int temp[]=new int[maxDigit];
+		for(int i=0;i<digits.length;i++){
+			temp[i]=digits[i];
+		}
+		digits=temp;
+	}
+	digits[index]=number%10;
+	number=number/10;
+	index++;
+	}
+
+	//Calculating largest and second Largest
+	int largest=0;
+	int secondLargest=0;
+	for(int i=0;i<index;i++){
+		if(digits[i]>largest){
+			secondLargest=largest;
+			largest=digits[i];
+		}
+		else if(digits[i]>secondLargest&&digits[i]!=largest){
+			secondLargest=digits[i];
+		}
+	}
+
+	//Dsiplaying largest and Second Largest
+	System.out.println(largest);
+	System.out.println(secondLargest);
+	}
+}
