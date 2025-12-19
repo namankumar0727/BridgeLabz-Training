bb656601157f1ffd2e6dae24f2220eb142904941
Naman Kumar <namankumar0727@gmail.com>
2025-12-20 01:26:47 +0530

    [Naman] : added Reverse Number

---
+++ gcr-codebase/arrays/Level2/ReverseNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class ReverseNumber{
+public static void main(String args[]){
+	
+	//Taking input of number
+	Scanner sc=new Scanner(System.in);
+	int number=sc.nextInt();
+
+	//Reversing and storing in array
+	int temp=number;
+	int count=0;
+	while(temp!=0){
+		count++;
+		temp=temp/10;
+	}
+	int digits[]=new int[count];
+	temp=number;
+	for(int i=count-1;i>=0;i--){
+		digits[i]=temp%10;
+		temp=temp/10;
+	}
+	//Displaying the reverse number
+	for(int i=0;i<count;i++){
+		System.out.print(digits[i]);
+	}
+	}
+}
