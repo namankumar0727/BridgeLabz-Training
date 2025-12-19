01d3b869f6e882f8b83615c2ecba0ee4a92cb84d
Naman Kumar <namankumar0727@gmail.com>
2025-12-20 01:27:16 +0530

    [Naman] : added Tallest and Youngest

---
+++ gcr-codebase/arrays/Level2/Tallestandyoungest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Tallestandyoungest{
+	public static void main(String args[]){
+	
+	//Taking user input in height and age
+	Scanner sc=new Scanner(System.in);
+	int age[]=new int[3];
+	double height[]=new double[3];
+	String names[]={"Amar","Akbar","Anthony"};
+	for(int i=0;i<3;i++){
+		age[i]=sc.nextInt();
+		height[i]=sc.nextDouble();
+	}
+	
+	//Calculating the youngest and tallest
+	int youngestIndex=0;
+	int tallestIndex=0;
+	for(int i=1;i<3;i++){
+		if(age[i]<age[youngestIndex]){
+			youngestIndex=i;
+		}
+	if(height[i]>height[tallestIndex]){
+		tallestIndex=i;
+	}
+	}
+	
+	//Displaying the youngest and the tallest among them
+	System.out.println("Youngest -> "+names[youngestIndex]);
+	System.out.println("Tallest -> "+names[tallestIndex]);
+	}
+}
