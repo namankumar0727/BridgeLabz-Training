a93c218cb3cb5e3f326e19f924298ebc6ff8fa04
Naman Kumar <namankumar0727@gmail.com>
2025-12-22 01:01:27 +0530

    [Naman] : added FootBall Team Heights

---
+++ gcr-codebase/Methods/Level3/FootballTeamHeights.java
@@ -0,0 +1,53 @@
+public class FootballTeamHeights{
+
+    //Method to find sum of heights
+    public static int findSum(int heights[]){
+        int sum=0;
+        for(int i=0;i<heights.length;i++){
+            sum=sum+heights[i];
+        }
+        return sum;
+    }
+    
+    //method to find the mean height
+    public static double findMean(int heights[]){
+        return (double)findSum(heights)/heights.length;
+    }
+    
+    //method to find the shortest height
+    public static int findShortest(int heights[]){
+        int min=heights[0];
+        for(int i=1;i<heights.length;i++){
+            if(heights[i]<min){
+                min=heights[i];
+            }
+        }
+        return min;
+    }
+    
+    //method to find tallest height
+    public static int findTallest(int heights[]){
+        int max=heights[0];
+        for(int i=1;i<heights.length;i++){
+            if(heights[i]>max){
+                max=heights[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[]args){
+        
+        int heights[]=new int[11];
+        
+        //Generating random heights between 150 and 250
+        for(int i=0;i<11;i++){
+            heights[i]=(int)(Math.random()*101)+150;
+        }
+        
+        //Displaying the desired results
+        System.out.println("Shortest Height: "+findShortest(heights));
+        System.out.println("Tallest Height: "+findTallest(heights));
+        System.out.println("Mean Height: "+findMean(heights));
+    }
+}
