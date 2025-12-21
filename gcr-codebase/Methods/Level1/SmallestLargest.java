4ca2930b114f0b722569c655d63ec74c071e60b3
Naman Kumar <namankumar0727@gmail.com>
2025-12-22 00:42:30 +0530

    [Naman] : added Smallest and Largest

---
+++ gcr-codebase/Methods/Level1/SmallestLargest.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class FindSmallestAndLargest{
+
+    public static void main(String[]args){
+        
+        //Taking user inputs for the three Numbers
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter 3 Numbers: ");
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        
+        //Calling method findSmallestAndLargest for calculating smallest and largest
+        int result[]=findSmallestAndLargest(num1,num2,num3);
+        
+        //Displaying results
+        System.out.println("Smallest number - "+result[0]);
+        System.out.println("Largest number - "+result[1]);
+    }
+    
+    //Method to find smallest and largest Number
+    public static int[] findSmallestAndLargest(int num1,int num2,int num3){
+        int smallest=num1;
+        int largest=num1;
+        
+        if(number2<smallest){
+            smallest=num2;
+        }
+        if(number3<smallest){
+            smallest=num3;
+        }
+        
+        if(number2>largest){
+            largest=num2;
+        }
+        if(number3>largest){
+            largest=num3;
+        }
+        
+        return new int[]{smallest,largest};
+    }
+}
