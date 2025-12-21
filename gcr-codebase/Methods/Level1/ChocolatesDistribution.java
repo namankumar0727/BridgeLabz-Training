8b17784beb2b23ac5e0c89cef994864c11c3d004
Naman Kumar <namankumar0727@gmail.com>
2025-12-22 00:40:06 +0530

    [Naman] : added Chocolaates Distribution

---
+++ gcr-codebase/Methods/Level1/ChocolatesDistribution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class ChocolatesDistribution{
+	
+	//Method for calculating chocolate distribution
+	public static int[] findDistribution(int chocolates,int children){
+		int each=chocolates/children;
+		int remaining=chocolates%children;
+		return new int[]{each,remaining};
+	}
+	
+	public static void main(String args[]){
+		
+		//Taking input from the user for chocolates and children
+		Scanner sc=new Scanner(System.in);
+		int chocolates=sc.nextInt();
+		int children=sc.nextInt();
+		
+		//Calling method findDistribution for calculating
+		int result[]=findDistribution(chocolates,children);
+		
+		//Printing the number of chocolates each get and remaining chocolates as well
+		System.out.println("Each will get "+result[0]);
+		System.out.println("Remaining Chocolates "+result[1]);
+	}
+}
+
