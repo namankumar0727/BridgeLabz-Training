15f4096c9864677e1a97847d52da788ec23bf1ca
Naman Kumar <namankumar0727@gmail.com>
2025-12-22 17:25:55 +0530

    [Naman] : added Word Length

---
+++ gcr-codebase/Strings/Level2/WordLength.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class WordLength{
+
+	//Method for length
+	static int len(String s){
+		int c=0;try{while(true){s.charAt(c++);}}catch(Exception e){}
+		return c-1;
+	}
+	
+	//Method for word length
+	static String[][] wordLength(String words[]){
+		String r[][]=new String[words.length][2];
+		for(int i=0;i<words.length;i++){
+			r[i][0]=words[i];
+			r[i][1]=String.valueOf(len(words[i]));
+		}
+	return r;
+	}
+	public static void main(String args[]){
+	
+		//Taking input from the user
+		Scanner sc=new Scanner(System.in);
+		String text=sc.nextLine();
+		String w[]=text.split(" ");
+		String r[][]=wordLength(w);
+		
+		//Displaying desired output
+		for(int i=0;i<r.length;i++)
+		System.out.println(r[i][0]+" "+Integer.parseInt(r[i][1]));
+	}
+}
