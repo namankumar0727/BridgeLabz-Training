4669ee8e05c14e7798cd5a62f2200d7457bb821a
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2025-12-23 12:40:37 +0530

    [Naman] : added Multiplication Table Specific

--- gcr-codebase/arrays/Level1/MultiplicationTableSpecific.java
+++ gcr-codebase/arrays/Level1/MultiplicationTableSpecific.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class MultiplicationTableSpecific{
+public class MultiplicationTableSpecific{
 	public static void main(String args[]){
 	
 	//Taking input of the number and calculating the multiplication Table
