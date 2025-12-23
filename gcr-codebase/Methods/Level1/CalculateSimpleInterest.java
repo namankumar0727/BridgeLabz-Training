048738909bb79bc3ec15ecc0683c79ccb0e2f5a3
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2025-12-23 13:07:49 +0530

    [Naman] : added Calculate Simple Interest

--- gcr-codebase/Methods/Level1/CalculateSimpleInterest.java
+++ gcr-codebase/Methods/Level1/CalculateSimpleInterest.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-class CalculateSimpleInterest{
+public class CalculateSimpleInterest{
 
 	//Method to Calculate Simple Interest
-	static double calculateSI(double p,double r,double t){
+	public static double calculateSI(double p,double r,double t){
 		return (p*r*t)/100;
 	}
 	public static void main(String args[]){
