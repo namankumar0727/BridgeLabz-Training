2d870a942a70c22a9b5760daa0a305b9698c1511
Naman Kumar <namankumar0727@gmail.com>
2025-12-27 23:52:23 +0530

    [Naman] : added Circle

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/levelone/UseCircle.java
@@ -0,0 +1,14 @@
+package com.constructor.levelone;
+
+public class UseCircle {
+	    public static void main(String[] args) {
+
+	        // Using default constructor
+	        Circle c1 = new Circle();
+	        c1.display();
+
+	        // Using parameterized constructor
+	        Circle c2 = new Circle(8);
+	        c2.display();
+	    }
+	}
