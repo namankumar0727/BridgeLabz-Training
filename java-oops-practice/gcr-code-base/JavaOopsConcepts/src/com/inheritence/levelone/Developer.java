544719ff2a00e6e888e686f0104366f94505d8ce
Naman Kumar <namankumar0727@gmail.com>
2025-12-31 22:12:15 +0530

    [Naman] : added Employee Management System

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritence/levelone/Developer.java
@@ -0,0 +1,16 @@
+package com.inheritence.levelone;
+
+class Developer extends Employee {
+    String programmingLanguage;
+
+    Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritence/levelone/Intern.java
@@ -0,0 +1,18 @@
+package com.inheritence.levelone;
+
+class Intern extends Employee {
+    
+	//Declaring the variable
+	int duration;
+
+    Intern(String name, int id, double salary, int duration) {
+        super(name, id, salary);
+        this.duration = duration;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + duration + " months");
+    }
+}
