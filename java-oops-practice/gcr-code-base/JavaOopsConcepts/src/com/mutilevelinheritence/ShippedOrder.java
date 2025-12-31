f1c45718937ba979bf3c2846ff3be0d3a0ce089c
Naman Kumar <namankumar0727@gmail.com>
2025-12-31 22:19:00 +0530

    [Naman] : added Retail Order Management

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/mutilevelinheritence/ShippedOrder.java
@@ -0,0 +1,17 @@
+package com.mutilevelinheritence;
+
+public class ShippedOrder extends Order {
+	
+	//Declaring Variables
+    String trackingNumber;
+
+    ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order Shipped (Tracking No: " + trackingNumber + ")";
+    }
+}
