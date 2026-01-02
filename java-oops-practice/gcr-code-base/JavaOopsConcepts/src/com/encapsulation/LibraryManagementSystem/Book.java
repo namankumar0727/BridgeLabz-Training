f239d0b343a3639e43956bd41b9a5fc27060eee1
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-02 12:16:20 +0530

    [Naman] : added Library Management System

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/LibraryManagementSystem/Book.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/LibraryManagementSystem/Book.java
@@ -1,5 +1,30 @@
 package com.encapsulation.LibraryManagementSystem;
 
-public class Book {
+package com.encapsulationpolymorphisminterfaceandabstract.librarymanagementsystem;
 
+class Book extends LibraryItem implements Reservable {
+
+    public Book(int id, String title, String author) {
+        super(id, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 14; // 14 days
+    }
+
+    @Override
+    public void reserveItem(String borrowerName) {
+        if (checkAvailability()) {
+            setBorrower(borrowerName);
+            System.out.println("Book reserved by " + borrowerName);
+        } else {
+            System.out.println("Book not available");
+        }
+    }
+
+    @Override
+    public boolean checkAvailability() {
+        return isAvailable();
+    }
 }
