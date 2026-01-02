caa3d63ecbbcac915512fc07c53f861f0cb995fb
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-02 12:07:56 +0530

    [Naman] : added Banking System

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/BankingSystem/SavingsAccount.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/BankingSystem/SavingsAccount.java
@@ -1,5 +1,26 @@
 package com.encapsulation.BankingSystem;
 
-public class SavingsAccount {
+class SavingsAccount extends BankAccount implements Loanable {
 
+    private double loanAmount;
+
+    public SavingsAccount(String accNo, String name, double balance) {
+        super(accNo, name, balance);
+    }
+
+    @Override
+    public double calculateInterest() {
+        return balance * 0.04; // 4% interest
+    }
+
+    @Override
+    public void applyForLoan(double amount) {
+        loanAmount = amount;
+        System.out.println("Loan applied for: " + amount);
+    }
+
+    @Override
+    public boolean calculateLoanEligibility() {
+        return balance >= 5000;
+    }
 }
