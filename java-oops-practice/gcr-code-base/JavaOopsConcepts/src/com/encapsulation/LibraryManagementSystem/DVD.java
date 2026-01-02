4411527ca988c8bd984e323d051c8d3b489d0914
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-02 12:17:30 +0530

    [Naman] : added Library Management System

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/LibraryManagementSystem/DVD.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/LibraryManagementSystem/DVD.java
@@ -1,5 +1,28 @@
 package com.encapsulation.LibraryManagementSystem;
 
-public class DVD {
+class DVD extends LibraryItem implements Reservable {
 
+    public DVD(int id, String title, String author) {
+        super(id, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 3; // 3 days
+    }
+
+    @Override
+    public void reserveItem(String borrowerName) {
+        if (checkAvailability()) {
+            setBorrower(borrowerName);
+            System.out.println("DVD reserved by " + borrowerName);
+        } else {
+            System.out.println("DVD not available");
+        }
+    }
+
+    @Override
+    public boolean checkAvailability() {
+        return isAvailable();
+    }
 }
