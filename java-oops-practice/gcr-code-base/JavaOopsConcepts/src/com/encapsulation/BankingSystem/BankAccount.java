7ed76fd3bb47c467b00ad8bc2d2c0f77a19874f1
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-02 12:03:58 +0530

    [Naman] : added Banking System

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/BankingSystem/BankAccount.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/BankingSystem/BankAccount.java
@@ -1,5 +1,51 @@
 package com.encapsulation.BankingSystem;
 
-public class BankAccount {
+package com.encapsulationpolymorphisminterfaceandabstract.bankingsystem;
 
+abstract class BankAccount {
+
+    // Encapsulation private fields
+    private String accountNumber;
+    private String holderName;
+    protected double balance;   
+
+    // Constructor
+    public BankAccount(String accNo, String holderName, double balance) {
+        this.accountNumber = accNo;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
+
+    // Display method
+    public void displayAccountDetails() {
+        System.out.println("Account No : " + accountNumber);
+        System.out.println("Holder     : " + holderName);
+        System.out.println("Balance    : " + balance);
+    }
+
+    // Getter
+    public double getBalance() {
+        return balance;
+    }
+    
 }
