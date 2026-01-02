129e8111e68cb1cf96e562ef68a14dc24d8b2b79
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-02 12:29:14 +0530

    [Naman] : added Ride Hailing Application

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/RideHailingApplication/Bike.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/RideHailingApplication/Bike.java
@@ -1,5 +1,25 @@
 package com.encapsulation.RideHailingApplication;
 
-public class Bike {
+class Bike extends Vehicle implements GPS {
 
+    private String location;
+
+    public Bike(int id, String driver, double rate) {
+        super(id, driver, rate);
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return (ratePerKm * distance) * 0.9; // 10% cheaper
+    }
+
+    @Override
+    public void updateLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return location;
+    }
 }
