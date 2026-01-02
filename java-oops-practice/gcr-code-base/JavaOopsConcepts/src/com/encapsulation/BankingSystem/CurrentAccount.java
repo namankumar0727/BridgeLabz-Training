46f30323a8e302c886d6b5e911618fbcbccd0ce1
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-02 12:06:18 +0530

    [Naman] : added Banking System

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/BankingSystem/CurrentAccount.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/BankingSystem/CurrentAccount.java
@@ -1,5 +1,26 @@
 package com.encapsulation.BankingSystem;
 
-public class CurrentAccount {
+class CurrentAccount extends BankAccount implements Loanable {
 
+    private double loanAmount;
+    public CurrentAccount(String accNo, String name, double balance) {
+        super(accNo, name, balance);
+    }
+
+    @Override
+    public double calculateInterest() {
+    	// 2% interest
+        return balance * 0.02; 
+    }
+
+    @Override
+    public void applyForLoan(double amount) {
+        loanAmount = amount;
+        System.out.println("Loan applied for: " + amount);
+    }
+
+    @Override
+    public boolean calculateLoanEligibility() {
+        return balance >= 10000;
+    }
 }
