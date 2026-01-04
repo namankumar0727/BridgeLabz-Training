95b565029e9b9a1dbadf4733bbef43a54ddfd32f
Naman Kumar <namankumar0727@gmail.com>
2026-01-04 23:40:07 +0530

    [Naman] : added Longest Sequence

---
+++ DSA-Practice/gcr-codebase/JavaDsaPractice/src/com/hashmapandhashfunc/longestsequence/LongestConsecutiveSequence.java
@@ -0,0 +1,39 @@
+package com.hashmapandhashfunc.longestsequence;
+import java.util.HashSet;
+import java.util.Set;
+
+class LongestConsecutiveSequence {
+
+    public static int findLongestConsecutive(int[] arr) {
+
+        if (arr.length == 0)
+            return 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        // Add all elements to set
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+
+            // Check if num is the start of a sequence
+            if (!set.contains(num - 1)) {
+
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+        return longest;
+    }
+}
