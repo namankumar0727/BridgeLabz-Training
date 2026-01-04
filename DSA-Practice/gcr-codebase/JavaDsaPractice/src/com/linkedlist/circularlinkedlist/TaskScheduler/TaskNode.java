e09ec191870daab0475c6420bec11acc155e4e42
Naman Kumar <namankumar0727@gmail.com>
2026-01-04 23:30:05 +0530

    [Naman] : added Task Scheduler

---
+++ DSA-Practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/circularlinkedlist/TaskScheduler/TaskNode.java
@@ -0,0 +1,19 @@
+package com.linkedlist.circularlinkedlist.TaskScheduler;
+
+public class TaskNode {
+    int taskId;
+    String taskName;
+    int priority;
+    String dueDate;
+
+    TaskNode next;
+
+    //Constructor
+    TaskNode(int taskId, String taskName, int priority, String dueDate) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.next = null;
+    }
+}
