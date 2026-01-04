4e4e067f83301587a941b3f5d4f6a25c68d8a16c
Naman Kumar <namankumar0727@gmail.com>
2026-01-04 23:28:43 +0530

    [Naman] : added Online Ticket Reservation System

---
+++ DSA-Practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/circularlinkedlist/onlineticketreservationsystem/TicketReservationApp.java
@@ -0,0 +1,27 @@
+package com.linkedlist.circularlinkedlist.onlineticketreservationsystem;
+
+public class TicketReservationApp {
+    public static void main(String[] args) {
+
+        TicketCircularList tickets = new TicketCircularList();
+
+        tickets.addTicket(101, "Naman", "Inception", "A1", "10:00 AM");
+        tickets.addTicket(102, "abc", "Inception", "A2", "10:05 AM");
+        tickets.addTicket(103, "xyz", "Avatar", "B1", "10:10 AM");
+
+        tickets.displayTickets();
+
+        System.out.println("\n Search by Customer 'Naman':");
+        tickets.searchByCustomer("Ravi");
+
+        System.out.println("\n Search by Movie 'abc':");
+        tickets.searchByMovie("Inception");
+
+        System.out.println("\n Remove Ticket ID 102:");
+        tickets.removeTicket(102);
+        tickets.displayTickets();
+
+        System.out.println();
+        tickets.countTickets();
+    }
+}
