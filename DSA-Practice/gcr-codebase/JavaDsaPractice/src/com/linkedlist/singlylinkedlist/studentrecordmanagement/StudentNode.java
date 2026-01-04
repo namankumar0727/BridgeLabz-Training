3a85876381317497de9b4af2fb4b0c0f7176ef5e
Naman Kumar <namankumar0727@gmail.com>
2026-01-04 23:25:06 +0530

    [Naman] : added Student Record App

---
+++ DSA-Practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/singlylinkedlist/studentrecordmanagement/StudentNode.java
@@ -0,0 +1,20 @@
+package com.linkedlist.singlylinkedlist.studentrecordmanagement;
+
+public class StudentNode {
+	
+	//Declaration of the variables and data structure
+    int rollNo;
+    String name;
+    int age;
+    String grade;
+    StudentNode next;
+
+    //Constructor
+    StudentNode(int rollNo, String name, int age, String grade) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}
