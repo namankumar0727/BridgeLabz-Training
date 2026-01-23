41fec81fa6deed9c43a998a82fdae21431364c80
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-24 02:48:38 +0530

    [Naman] : Added Unchecked Exceptions

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/Exceptions/uncheckedexceptions/UncheckedExceptionExample.java
@@ -0,0 +1,31 @@
+package com.Exceptions.uncheckedexceptions;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UncheckedExceptionExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter numerator: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("Enter denominator: ");
+            int num2 = sc.nextInt();
+
+            int result = num1 / num2;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter numeric values only");
+
+        } finally {
+            sc.close();
+        }
+    }
+}
