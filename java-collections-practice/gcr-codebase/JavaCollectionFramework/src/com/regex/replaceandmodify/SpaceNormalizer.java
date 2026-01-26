30bba4cc917658f116325abee9edb060530d0fc8
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-26 20:50:31 +0530

    [Naman] : Added Space Normalizer

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/regex/replaceandmodify/SpaceNormalizer.java
@@ -0,0 +1,13 @@
+package com.regex.replaceandmodify;
+public class SpaceNormalizer {
+
+    public static void main(String[] args) {
+
+        String input = "This   is   an   example    with   multiple   spaces.";
+
+        // Replace multiple spaces with single space
+        String output = input.replaceAll("\\s+", " ");
+
+        System.out.println(output);
+    }
+}
