364cb6eb6c92e0fb6ae5cb3c62793a60e4d63bd0
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-27 00:22:01 +0530

    [Naman] : Added Access Private Field

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/reflection/basic/accesprivatefield/Person.java
@@ -0,0 +1,17 @@
+package com.reflection.basic.accesprivatefield;
+
+public class Person {
+
+    // Private field
+    private int age;
+
+    // Constructor
+    public Person() {
+        age = 18;
+    }
+
+    // Normal method (not used by reflection)
+    public int getAge() {
+        return age;
+    }
+}
