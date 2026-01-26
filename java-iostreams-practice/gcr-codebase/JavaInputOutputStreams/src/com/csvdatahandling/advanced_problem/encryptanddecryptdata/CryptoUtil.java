aa36f540e657650a16aa92a4cce814049faf068a
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-27 01:14:12 +0530

    [Naman] : Added Encrypt and Decrypt Data

---
+++ java-iostreams-practice/gcr-codebase/JavaInputOutputStreams/src/com/csvdatahandling/advanced_problem/encryptanddecryptdata/CryptoUtil.java
@@ -0,0 +1,16 @@
+package com.csvdatahandling.advanced_problem.encryptanddecryptdata;
+
+import java.util.Base64;
+
+public class CryptoUtil {
+
+    // Encrypt data using Base64
+    public static String encrypt(String data) {
+        return Base64.getEncoder().encodeToString(data.getBytes());
+    }
+
+    // Decrypt data using Base64
+    public static String decrypt(String encryptedData) {
+        return new String(Base64.getDecoder().decode(encryptedData));
+    }
+}
