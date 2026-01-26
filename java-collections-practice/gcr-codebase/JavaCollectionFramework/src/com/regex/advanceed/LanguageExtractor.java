ecf200a00504c8977b79ad81bfe66e554be71f8b
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-26 20:38:25 +0530

    [Naman] : Added Language Extractor

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/regex/advanceed/LanguageExtractor.java
@@ -0,0 +1,20 @@
+package com.regex.advanceed;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LanguageExtractor {
+
+    public static void main(String[] args) {
+
+        String text = "I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+
+        String regex = "\\b(Java|Python|JavaScript|Go)\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
