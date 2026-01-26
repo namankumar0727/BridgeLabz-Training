7de288b385594b0734cb862f8c4e997e06191ed2
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-27 01:34:36 +0530

    [Naman] : Added Car object to json convert

---
+++ java-iostreams-practice/gcr-codebase/JavaInputOutputStreams/src/com/json_handling/carobject_to_json/Car.java
@@ -0,0 +1,16 @@
+package com.json_handling.carobject_to_json;
+
+public class Car {
+
+    private String brand;
+    private String model;
+    private int year;
+    private double price;
+
+    public Car(String brand, String model, int year, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+}
