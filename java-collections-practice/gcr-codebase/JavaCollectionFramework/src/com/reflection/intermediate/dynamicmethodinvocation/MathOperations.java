3154c2ce52352e7b9f2c9754cccfcc866b84677d
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-01-27 00:26:47 +0530

    [Naman] : Dynamic Method Invocation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/reflection/intermediate/dynamicmethodinvocation/MathOperations.java
@@ -0,0 +1,16 @@
+package com.reflection.intermediate.dynamicmethodinvocation;
+
+public class MathOperations {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
