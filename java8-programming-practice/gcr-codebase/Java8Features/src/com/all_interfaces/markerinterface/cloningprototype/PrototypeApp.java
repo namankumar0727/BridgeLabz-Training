8b735a2dc0dd3595faf5b75608823713ee02794d
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-02-02 23:33:19 +0530

    [Naman] : Added Cloning Prototype

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/all_interfaces/markerinterface/cloningprototype/PrototypeApp.java
@@ -0,0 +1,20 @@
+package com.all_interfaces.markerinterface.cloningprototype;
+
+public class PrototypeApp {
+    public static void main(String[] args) {
+
+        try {
+            PrototypeModel original = new PrototypeModel("InvoiceTemplate", 1);
+
+            PrototypeModel copy1 = (PrototypeModel) original.clone();
+            PrototypeModel copy2 = (PrototypeModel) original.clone();
+
+            System.out.println("Original: " + original);
+            System.out.println("Clone 1: " + copy1);
+            System.out.println("Clone 2: " + copy2);
+
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Cloning not supported");
+        }
+    }
+}
