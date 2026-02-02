dfeb9dbc3864a5a7aa9ccd0156fa09ea95e5b8a0
Naman-Kumar <153828446+namankumar0727@users.noreply.github.com>
2026-02-03 00:03:30 +0530

    [Naman] : Added Invoice Object

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/lambdaexpressions/referencemethod/invoiceobject/Invoice.java
@@ -0,0 +1,14 @@
+package com.lambdaexpressions.referencemethod.invoiceobject;
+
+class Invoice {
+    int transactionId;
+
+    Invoice(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice generated for Transaction ID: " + transactionId;
+    }
+}
